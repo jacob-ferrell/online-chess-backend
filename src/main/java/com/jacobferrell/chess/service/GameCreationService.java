b4d3ab9ef7471b06c24a80b5136918b9d8b32bbe
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-05 10:07:01 -0400

    put createGame into own service to avoid circular dependency

---
+++ src/main/java/com/jacobferrell/chess/service/GameCreationService.java
@@ -0,0 +1,56 @@
+package com.jacobferrell.chess.service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import com.jacobferrell.chess.model.GameDTO;
+import com.jacobferrell.chess.model.UserDTO;
+import com.jacobferrell.chess.repository.GameRepository;
+import com.jacobferrell.chess.repository.UserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Service
+public class GameCreationService {
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    public GameDTO createGame(long p2, HttpServletRequest request) {
+        UserDTO player1 = jwtService.getUserFromRequest(request);
+        Optional<UserDTO> optionalPlayer2 = userRepository.findById(p2);
+        if (!optionalPlayer2.isPresent()) {
+            throw new NotFoundException("The provided user id do not exist");
+        }
+        //Randomly assign players to white/black
+        Set<UserDTO> players = new HashSet<>();
+        UserDTO player2 = optionalPlayer2.get();
+        players.add(player1);
+        players.add(player2);
+        double rand = Math.random();
+        UserDTO whitePlayer;
+        UserDTO blackPlayer;
+        if (rand >= .5) {
+            whitePlayer = player1;
+            blackPlayer = player2;
+        } else {
+            whitePlayer = player2;
+            blackPlayer = player1;
+        }
+        GameDTO newGame = GameDTO.builder().players(players).whitePlayer(whitePlayer).blackPlayer(blackPlayer).currentTurn(whitePlayer)
+                .winner(null)
+                .build();
+        gameRepository.save(newGame);
+        return newGame;
+    }
+}
