6bb980dfa37ed994b5a46ef93c622775fa951c48
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-05 10:06:38 -0400

    added query for finding users that are waiting in lobby

--- src/main/java/com/jacobferrell/chess/repository/UserRepository.java
+++ src/main/java/com/jacobferrell/chess/repository/UserRepository.java
@@ -1,14 +1,18 @@
 package com.jacobferrell.chess.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jacobferrell.chess.model.UserDTO;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserDTO, Long> {
     Optional<UserDTO> findByEmail(String email);
+
+    @Query("SELECT u FROM UserDTO u WHERE u.inLobby = true")
+    Set<UserDTO> findByInLobby();
 }
