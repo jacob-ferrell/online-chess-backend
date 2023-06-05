09628acaf52937f0f6742c25e422784e36a16d1e
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-05 10:05:46 -0400

    added route for joining lobby

--- src/main/java/com/jacobferrell/chess/controller/UserController.java
+++ src/main/java/com/jacobferrell/chess/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,11 @@ public class UserController {
     @GetMapping("/friends")
     ResponseEntity<?> getFriends(HttpServletRequest request) {
         return ResponseEntity.ok().body(userService.getFriends(request));
+    }
+
+    @PutMapping("/lobby")
+    ResponseEntity<?> joinLobby(HttpServletRequest request) {
+        return ResponseEntity.ok().body(userService.joinLobby(request));
     }
 
 }
