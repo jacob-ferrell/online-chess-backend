5ba7ea9c2107d858ce4f24742d05a393b80f216a
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-11 19:43:47 -0400

    moved makeMove method from MoveService into it's own service to avoid circular dependencies

---
+++ src/main/java/com/jacobferrell/chess/service/MoveCreationService.java
@@ -0,0 +1,68 @@
+package com.jacobferrell.chess.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jacobferrell.chess.game.Game;
+import com.jacobferrell.chess.model.GameDTO;
+import com.jacobferrell.chess.model.MoveDTO;
+import com.jacobferrell.chess.model.Role;
+import com.jacobferrell.chess.model.UserDTO;
+import com.jacobferrell.chess.pieces.ChessPiece;
+import com.jacobferrell.chess.pieces.PieceColor;
+import com.jacobferrell.chess.repository.GameRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Service
+public class MoveCreationService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private MoveService moveService;
+
+    public Map<String, Object> makeMove(long gameId, int x0, int y0, int x1, int y1, HttpServletRequest request) {
+        GameDTO gameData = moveService.getGameById(gameId);
+        UserDTO user = jwtService.getUserFromRequest(request);
+        moveService.validateGameIncludesPlayer(gameData, user);
+        moveService.validateGameIsNotOver(gameData);
+        moveService.validateIsPlayersTurn(gameData, user);
+        PieceColor playerColor = moveService.getPlayerColor(gameData, user);
+        // Convert game object from frontend into backend game object
+        Game game = moveService.createGameFromDTO(gameData);
+        // Test that a piece has been selected and belongs to the current user
+        ChessPiece selectedPiece = moveService.getAndValidatePiece(x0, y0, game, user, playerColor);
+        // Create move object and simulate the move to see if it is legal
+        moveService.validateAndMakeMove(selectedPiece, x1, y1, game.board);
+        // Set and save the board, moves, turn, and playerInCheck
+        MoveDTO move = moveService.createMoveDTO(selectedPiece, playerColor, x0, y0, x1, y1);
+        gameData.setPieces(game.board.getPieceData());
+
+        Set<MoveDTO> moves = gameData.getMoves();
+        moves.add(move);
+
+        moveService.switchTurns(gameData);
+
+        moveService.setPlayerInCheck(game, gameData, user);
+        gameRepository.save(gameData);
+
+        // Send message to websocket with updated game state and notification
+        // so that if other player is connected, the notifcation can automatically
+        // be marked as read
+        moveService.sendMessageAndNotification(user, gameData, request);
+        Map<String, Object> moveData = new HashMap<>();
+        moveData.put("gameData", gameData);
+        moveData.put("moveData", move);
+        return moveData;
+
+    }
+}
