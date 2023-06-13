92dc4ac2334cc75368718649c546e780a2a5dd56
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-12 21:43:17 -0400

    updated to take Position instead of ints for coordinates

--- src/test/java/com/jacobferrell/chess/CastleTest.java
+++ src/test/java/com/jacobferrell/chess/CastleTest.java
@@ -5,25 +5,26 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import com.jacobferrell.chess.chessboard.ChessBoard;
+import com.jacobferrell.chess.chessboard.Position;
 import com.jacobferrell.chess.pieces.PieceColor;
 import com.jacobferrell.chess.pieces.Rook;
 
 public class CastleTest {
-    // Test getCastleRooks for king side 
+    // Test getCastleRooks for king side
     @Test
     public void testKingSide() {
         ChessBoard board = new ChessBoard();
-        board.removePieceAtPosition(5, 0);
-        board.removePieceAtPosition(6, 0);
-        board.removePieceAtPosition(5, 7);
-        board.removePieceAtPosition(6, 7);
+        board.removePieceAtPosition(new Position(5, 0));
+        board.removePieceAtPosition(new Position(6, 0));
+        board.removePieceAtPosition(new Position(5, 7));
+        board.removePieceAtPosition(new Position(6, 7));
         System.out.println(board);
         Set<Rook> white = board.getCastleRooks(PieceColor.WHITE);
         Set<Rook> black = board.getCastleRooks(PieceColor.BLACK);
-        for (Rook i: white) {
+        for (Rook i : white) {
             System.out.println(i);
         }
-        for (Rook i: black) {
+        for (Rook i : black) {
             System.out.println(i);
         }
     }
