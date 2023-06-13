e31becde74fe54c2599f9c54f4b8fac7f45fa506
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-12 21:35:50 -0400

    board no longer has to be passed to simulate move

--- src/main/java/com/jacobferrell/chess/pieces/Move.java
+++ src/main/java/com/jacobferrell/chess/pieces/Move.java
@@ -1,4 +1,7 @@
 package com.jacobferrell.chess.pieces;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.jacobferrell.chess.chessboard.*;
 
 public class Move {
@@ -14,10 +17,10 @@ public class Move {
         return piece;
     }
 
-    public ChessBoard simulateMove(ChessBoard board) {
-        ChessBoard clonedBoard = board.getClone();
-        ChessPiece pieceToMove = piece.getClone(clonedBoard);
-        clonedBoard.setPieceAtPosition(position.x, position.y, pieceToMove);
+    public ChessBoard simulateMove() {
+        ChessBoard clonedBoard = piece.board.getClone();
+        ChessPiece pieceToMove = clonedBoard.getPieceAtPosition(piece.position);
+        clonedBoard.setPieceAtPosition(position, pieceToMove);
         return clonedBoard;
     }
 
@@ -28,8 +31,39 @@ public class Move {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(piece.getName() + ": [" + position.y + ", " + position.x + "]");
+        sb.append(piece.getColor() + " " + piece.getName() + ": " + convertToChessCoordinates(position));
         return sb.toString();
     }
+
+    public static Set<Move> removeMovesIntoCheck(Set<Move> moves) {
+        return moves.stream().filter(move -> {
+            return move.isLegal(move.simulateMove());
+        }).collect(Collectors.toSet());
+    }
+
+    public static String convertToChessCoordinates(Position position) {
+        int x = position.x;
+        int y = position.y;
+        if (x < 0 || x > 7 || y < 0 || y > 7) {
+            throw new IllegalArgumentException("Invalid coordinates");
+        }
+    
+        char xChar = (char) ('A' + x);
+        int yInt = 8 - y;
+    
+        return Character.toString(xChar) + yInt;
+    }
+
+    /* @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Move)) {
+            return false;
+        }
+        Move m = (Move) o;
+        return m.piece.equals(piece) && m.position.equals(position);
+    } */
     
 }
